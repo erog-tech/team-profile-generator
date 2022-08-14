1240c190e18572404dc69a12af1db827772e8633
erog-tech <prikshitthakur0@gmail.com>
2022-08-14 14:21:52 +0200

    add test for employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,3 +4,47 @@ test("initiate employee", () => {
     const employee = new Employee();
     expect(typeof employee).toBe('object');
   });
+
+test("test the name", () => {
+    const name = "Employee1"
+    const employee = new Employee(name);
+    expect(employee.name).toBe(name);
+
+});
+test("test the id", () => {
+  const id = 122
+  const employee = new Employee(id);
+  expect(employee.id).toBe(id);
+
+});
+
+test("test the email", () => {
+  const email = "employee@gmail.com"
+  const employee = new Employee(email);
+  expect(employee.email).toBe(email);
+
+});
+test("test if it returns getName() ", () => {
+  const name = "Employee1"
+  const employee = new Employee(name);
+  expect(employee.getName).toBe(name);
+
+});
+test("test if it returns getId()", () => {
+  const name = "Employee1"
+  const employee = new Employee(name);
+  expect(employee.name).toBe(name);
+
+});
+test("test the name", () => {
+  const name = "Employee1"
+  const employee = new Employee(name);
+  expect(employee.name).toBe(name);
+
+});
+test("test the name", () => {
+  const name = "Employee1"
+  const employee = new Employee(name);
+  expect(employee.name).toBe(name);
+
+});
