03e6cf32401d2ca378ca266647c1a3906b06bf09
erog-tech <prikshitthakur0@gmail.com>
2022-08-14 15:41:27 +0200

    add the test for inter

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,7 +1,23 @@
 const Intern = require("../lib/Intern");
 
+// test fór schoolname
 test("check for schoolname", () => {
     const schoolname = "testschoolname";
     const intern = new Intern('interntest', 2132, 'intern@gmial.com', schoolname);
     expect(intern.school).toBe(schoolname)
 });
+
+// test for schoolname method
+test("test if getSchool() returns the name of school", () => {
+    const schoolname = "testschoolname";
+    const intern = new Intern('interntest', 2132, 'intern@gmial.com', schoolname);
+    expect(intern.getSchool()).toBe(schoolname); 
+});
+
+// test the getrole() method 
+test("test if getRole() returns role ", () => {
+    const role = "Intern"
+    const intern = new Intern(role);
+    expect(intern.getRole()).toBe(role);
+  
+  });
