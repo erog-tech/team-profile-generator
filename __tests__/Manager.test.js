f24de3c92616ebab91439547d935ff5ea4f1c9fb
erog-tech <prikshitthakur0@gmail.com>
2022-08-14 15:44:34 +0200

    add the test for manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,23 @@
 const Manager = require("../lib/Manager");
 
+// test for officenumber property
 test("check for Manager office no", () => {
     const officeno = 12414;
     const manager = new Manager('Managertest', 2133434, 'manager@gmial.com', officeno);
     expect(manager.officeNumber).toBe(officeno);
 });
+
+//  test for office number method
+test("check for Manager office no", () => {
+    const officeno = 12414;
+    const manager = new Manager('Managertest', 2133434, 'manager@gmial.com', officeno);
+    expect(manager.getOfficeNumber()).toBe(officeno);
+});
+
+// test the getrole() method 
+test("test if getRole() returns role ", () => {
+    const role = "Manager"
+    const manager = new Manager(role);
+    expect(manager.getRole()).toBe(role);
+  
+  });
